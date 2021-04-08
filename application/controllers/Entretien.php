167f55cb9a45366088a0018c417f518e46336676
Youssefbenmiled <75643217+Youssefbenmiled@users.noreply.github.com>
2021-04-09 00:38:57 +0100

    affectation api

--- application/controllers/Entretien.php
+++ application/controllers/Entretien.php
@@ -12,52 +12,43 @@ class Entretien extends REST_Controller
 		);
 
 
-		$create = $this->Model_generale->add_fn($data,"entretien");
-		if($create)
-		{
-			$res = array
-			(
+		$create = $this->Model_generale->add_fn($data, "entretien");
+		if ($create) {
+			$res = array(
 				'erorer' => false,
 				'msg' => "Ajouté avec success"
 			);
 			$this->response($res, REST_Controller::HTTP_OK);
-		}
-		else
-		{
-			$res= array(
+		} else {
+			$res = array(
 				'erorer' => true,
-				'msg' =>" Erreur l'ore l'insertion dans la base de donnée"
+				'msg' => " Erreur l'ore l'insertion dans la base de donnée"
 			);
 			$this->response($res, REST_Controller::HTTP_NOT_FOUND);
 		}
-
 	}
-	/*public function update_entretien_post()
+	public function update_entretien_post()
 	{
 		$data = array(
 			'id_voiture' => $this->input->post('id_voiture'),
-		'date' => $this->input->post('date'),
+			'date' => $this->input->post('date'),
 			'description' => $this->input->post('description')
 		);
 
-		$id=$this->input->post('id',true);
-		$update = $this->Model_generale->update_fn_bay_id($id,$data,"entretien","id_entretien");
-		if($update)
-		{
-			$res = array
-			(
+		$id = $this->input->post('id', true);
+		$update = $this->Model_generale->update_fn_bay_id($id, $data, "entretien", "id_entretien");
+		if ($update) {
+			$res = array(
 				'erorer' => false,
 				'msg' => "Modification avec succès"
 			);
 			$this->response($res, REST_Controller::HTTP_OK);
-		}
-		else
-		{
-			$res= array(
+		} else {
+			$res = array(
 				'erorer' => true,
-				'msg' =>" Erreur l'ore Modification dans la base de donnée"
+				'msg' => " Erreur l'ore Modification dans la base de donnée"
 			);
 			$this->response($res, REST_Controller::HTTP_NOT_FOUND);
 		}
-	}*/
+	}
 }
--- application/models/Model_affecter_v_chauffeur.php
+++ application/models/Model_affecter_v_chauffeur.php
@@ -5,7 +5,7 @@ class Model_affecter_v_chauffeur extends CI_Model
 {
 	public function get_affectation($statut)
 	{
-		$sql = "SELECT v.matricule,v.type,c.nomPrenom,avc.id_affecter_v_chauffeur 
+		$sql = "SELECT v.matricule,v.type,c.nomPrenom,avc.id_affecter_v_chauffeur,avc.id_voiture,avc.id_chauffeur
 		from chauffeur c JOIN affecter_v_chauffeur avc join voiture v 
 		on c.id_chauffeur=avc.id_chauffeur and avc.id_voiture = v.id_voiture 
 		WHERE avc.statut=" . $statut;
