a31b2a254b4660f5f71a474c4452bcf80893780f
Youssefbenmiled <75643217+Youssefbenmiled@users.noreply.github.com>
2021-04-09 23:45:46 +0100

    commit

--- application/controllers/Entretien.php
+++ application/controllers/Entretien.php
@@ -3,6 +3,14 @@
 require APPPATH . 'libraries/REST_Controller.php';
 class Entretien extends REST_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		Header('Access-Control-Allow-Origin: *'); //for allow any domain, insecure
+		Header('Access-Control-Allow-Headers: *'); //for allow any headers, insecure
+		Header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE'); //method allowed
+		$this->load->model("Model_entretien");
+	}
 	public function add_entretien_post()
 	{
 		$data = array(
@@ -48,6 +56,47 @@ class Entretien extends REST_Controller
 				'erorer' => true,
 				'msg' => " Erreur l'ore Modification dans la base de donnée"
 			);
+			$this->response($res, REST_Controller::HTTP_NOT_FOUND);
+		}
+	}
+
+	public function get_entretien_info_Get()
+	{
+		$data = $this->Model_entretien->get_entritien_ifo();
+		$total = count($data);
+		if ($total != 0) {
+			$res = array(
+				'erorer' => false,
+				'msg' => $data
+
+			);
+			$this->response($res, REST_Controller::HTTP_OK);
+		} else {
+			$res = array(
+				'erorer' => true,
+				'msg' => "Pas Des Donne "
+			);
+
+			$this->response($res, REST_Controller::HTTP_NOT_FOUND);
+		}
+	}
+	public function get_one_entretien_info_Get()
+	{
+		$data = $this->Model_entretien->get_one_entritien_ifo($this->input->get('id'));
+		$total = count($data);
+		if ($total != 0) {
+			$res = array(
+				'erorer' => false,
+				'msg' => $data
+
+			);
+			$this->response($res, REST_Controller::HTTP_OK);
+		} else {
+			$res = array(
+				'erorer' => true,
+				'msg' => "Pas Des Donne "
+			);
+
 			$this->response($res, REST_Controller::HTTP_NOT_FOUND);
 		}
 	}
--- application/models/Model_entretien.php
+++ application/models/Model_entretien.php
@@ -3,5 +3,18 @@
 
 class Model_entretien extends CI_Model
 {
+	public function get_entritien_ifo()
+	{
 
+		$sql = "SELECT * FROM entretien e join voiture v on e.id_voiture = v.id_voiture ORDER by e.date DESC";
+		$query = $this->db->query($sql);
+		return $query->result_array();
+	}
+	public function get_one_entritien_ifo($id)
+	{
+
+		$sql = "SELECT * FROM entretien e join voiture v on e.id_voiture = v.id_voiture WHERE e.id_entretien= " . $id;
+		$query = $this->db->query($sql);
+		return $query->result_array();
+	}
 }
