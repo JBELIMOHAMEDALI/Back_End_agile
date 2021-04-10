28b18f1fe5beffd5680c45d5531c766be8f131cc
jbeli <mohem12ali@gmail.com>
2021-04-10 21:28:29 +0200

    Carnet_bord

--- application/controllers/Carnet_bord.php
+++ application/controllers/Carnet_bord.php
@@ -3,6 +3,14 @@
 
 class Carnet_bord extends REST_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		Header('Access-Control-Allow-Origin: *'); //for allow any domain, insecure
+		Header('Access-Control-Allow-Headers: *'); //for allow any headers, insecure
+		Header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE'); //method allowed
+		$this->load->model("Modele_carnet_bord");
+	}
 	/*
 		 * ajouter une carnet_bord dans la bd
 		 * */
@@ -12,7 +20,7 @@ class Carnet_bord extends REST_Controller
 		$data = array(
 			'klm' => $this->input->post('klm'),
 			'consomation' => $this->input->post('consomation'),
-			'date' => $this->input->post('date'),
+			'date' =>  date('Y-m-d'),
 			'id_choufer' =>  $this->input->post('id_choufer'),
 		);
 			$create = $this->Model_generale->add_fn($data,"carnet_bord");
@@ -34,7 +42,33 @@ class Carnet_bord extends REST_Controller
 				$this->response($res, REST_Controller::HTTP_NOT_FOUND);
 			}
 
+	}//
+	public function get_all_carnet_bord_for_chouffeur_Get()
+	{
+		$id = $this->input->get('id', TRUE);
+		$data = $this->Modele_carnet_bord->get_carnet_bord_for_user($id);
+		$total = count($data);
+		if ($total != 0) {
+			$res = array(
+				'erorer' => false,
+				'msg' => $data
+
+			);
+			$this->response($res, REST_Controller::HTTP_OK);
+		} else {
+			$res = array(
+				'erorer' => true,
+				'msg' => "Pas Des Donne "
+			);
+
+			$this->response($res, REST_Controller::HTTP_NOT_FOUND);
+		}
 	}
+
+
+
+
+
 	/*
 	 * modifer une carnet_bord => tt les donne pour carnet_bord
 	 * */
