c96dc2dfb2e86230c0821a7c130d9b076a557591
Youssefbenmiled <75643217+Youssefbenmiled@users.noreply.github.com>
2021-04-13 23:16:24 +0100

    mission commit

--- application/controllers/Mission.php
+++ application/controllers/Mission.php
@@ -16,81 +16,71 @@ class Mission extends REST_Controller
 	 * */
 	public function add_mission_post()
 	{
-		$date_debut=$this->input->post('date_debut');
-		$date_fin=$this->input->post('date_fin');
-		$date1=date_create($date_debut);
-		$date2=date_create($date_fin);
-		$diff=date_diff($date1,$date2);
-		$d= $diff->format("%a");
+		$date_debut = $this->input->post('date_debut');
+		$date_fin = $this->input->post('date_fin');
+		$date1 = date_create($date_debut);
+		$date2 = date_create($date_fin);
+		$diff = date_diff($date1, $date2);
+		$d = $diff->format("%a");
 
 		$data = array(
 			'id_chefService' => $this->input->post('id_chefService'),
 			'id_chauffeur' => $this->input->post('id_chauffeur'),
 			'id_voiture ' => $this->input->post('id_voiture'),
-			'date_debut' => $date_debut,
-			'description' =>$this->input->post('description'),
-			'date_fin' =>$date_fin,
-			'periode_estem' =>$d,
+			'date_debut' => $this->input->post('date_debut'),
+			'description' => $this->input->post('description'),
+			'date_fin' => $this->input->post('date_fin'),
+
 		);
-		$create = $this->Model_generale->add_fn($data,"mission");
-		if($create)
-		{
-			$res = array
-			(
+		$create = $this->Model_generale->add_fn($data, "mission");
+		if ($create) {
+			$res = array(
 				'erorer' => false,
 				'msg' => "Ajouté avec success"
 			);
 			$this->response($res, REST_Controller::HTTP_OK);
-		}
-		else
-		{
-			$res= array(
+		} else {
+			$res = array(
 				'erorer' => true,
-				'msg' =>"Ajouté n'a pas réussi"
+				'msg' => "Ajouté n'a pas réussi"
 			);
 			$this->response($res, REST_Controller::HTTP_NOT_FOUND);
 		}
-
-
 	}
 	/*
 	 * update mission in db
 	 * */
 	public function update_mission_post()
 	{
-		$date_debut= $this->input->post('date_debut');
-		$date_fin=$this->input->post('date_fin');
-		$date1=date_create($date_debut);
-		$date2=date_create($date_fin);
-		$diff=date_diff($date1,$date2);
-		$d= $diff->format("%a");
+		$date_debut = $this->input->post('date_debut');
+		$date_fin = $this->input->post('date_fin');
+		$date1 = date_create($date_debut);
+		$date2 = date_create($date_fin);
+		$diff = date_diff($date1, $date2);
+		$d = $diff->format("%a");
 
 		$data = array(
 			'id_chefService' => $this->input->post('id_chefService'),
 			'id_chauffeur' => $this->input->post('id_chauffeur'),
 			'id_voiture ' => $this->input->post('id_voiture'),
-			'date_debut' => $date_debut,
-			'description' =>$this->input->post('description'),
-			'date_fin' =>$date_fin,
-			'periode_estem' =>$d,
+			'date_debut' => $this->input->post('date_debut'),
+			'description' => $this->input->post('description'),
+			'date_fin' => $this->input->post('date_fin'),
+
 		);
 
-		$id=$this->input->post('id',true);
-		$update = $this->Model_generale->update_fn_bay_id($id,$data,"mission","id_mission");
-		if($update)
-		{
-			$res = array
-			(
+		$id = $this->input->post('id', true);
+		$update = $this->Model_generale->update_fn_bay_id($id, $data, "mission", "id_mission");
+		if ($update) {
+			$res = array(
 				'erorer' => false,
 				'msg' => "Modification avec succès"
 			);
 			$this->response($res, REST_Controller::HTTP_OK);
-		}
-		else
-		{
-			$res= array(
+		} else {
+			$res = array(
 				'erorer' => true,
-				'msg' =>"Modification n'a pas réussi"
+				'msg' => "Modification n'a pas réussi"
 			);
 			$this->response($res, REST_Controller::HTTP_NOT_FOUND);
 		}
@@ -100,30 +90,70 @@ class Mission extends REST_Controller
 	 */
 	public function don_fonction_post()
 	{
-		
+
 		// $data=$this->Model_mission->get_date_debut($id);
 		//date avec heur
 		// $date_deb=date_create($data[0]->date_debut);
 		// $date_fn=date_create(date("Y-m-d"));
 		// $diff=date_diff($date_deb,$date_fn);
 		// $d= $diff->format("%a");
-		$id= $this->input->post('id');
+		$id = $this->input->post('id');
 		$update = $this->Model_mission->don_fn($id);
-		if($update)
-		{
-			$res = array
-			(
+		if ($update) {
+			$res = array(
 				'erorer' => false,
 				'msg' => "Finir Mission"
 			);
 			$this->response($res, REST_Controller::HTTP_OK);
-		}
-		else
-		{
-			$res= array(
+		} else {
+			$res = array(
 				'erorer' => true,
-				'msg' =>"Failer "
+				'msg' => "Failer "
 			);
+			$this->response($res, REST_Controller::HTTP_NOT_FOUND);
+		}
+	}
+
+
+	public function get_mission_info_all_get()
+	{
+
+		$data = $this->Model_mission->get_mession_info();
+		$total = count($data);
+		if ($total != 0) {
+			$res = array(
+				'erorer' => false,
+				'msg' => $data
+
+			);
+			$this->response($res, REST_Controller::HTTP_OK);
+		} else {
+			$res = array(
+				'erorer' => true,
+				'msg' => "Pas Des Donne "
+			);
+
+			$this->response($res, REST_Controller::HTTP_NOT_FOUND);
+		}
+	}
+
+	public function get_mission_info_one_get()
+	{
+		$id = $this->input->get('id', TRUE);
+		$data = $this->Model_mission->get_one_mession_info($id);
+		$total = count($data);
+		if ($total != 0) {
+			$res = array(
+				'erorer' => false,
+				'msg' => $data
+			);
+			$this->response($res, REST_Controller::HTTP_OK);
+		} else {
+			$res = array(
+				'erorer' => true,
+				'msg' => "Pas Des Donnees "
+			);
+
 			$this->response($res, REST_Controller::HTTP_NOT_FOUND);
 		}
 	}
--- application/models/Model_mission.php
+++ application/models/Model_mission.php
@@ -6,17 +6,31 @@ class Model_mission extends CI_Model
 	public function don_fn($id)
 	{
 
-		$date=date("Y-m-d H:i:s");
-		$d=strval(date("Y-m-d H:i:s"));
-		$sql="UPDATE mission SET etat=1 , date_fin_real= '".$d."'  WHERE id_mission = ".$id;
+		$date = date("Y-m-d H:i:s");
+		$d = strval(date("Y-m-d H:i:s"));
+		$sql = "UPDATE mission SET etat=1 , date_fin_real= '" . $d . "'  WHERE id_mission = " . $id;
 		$query = $this->db->query($sql);
-		return $this->db->affected_rows() > 0 ;
-		
+		return $this->db->affected_rows() > 0;
 	}
 	public function get_date_debut($id)
 	{
-		$sql="SELECT mission.date_debut  FROM mission WHERE mission.id_mission= ".$id;
+		$sql = "SELECT mission.date_debut  FROM mission WHERE mission.id_mission= " . $id;
 		$query = $this->db->query($sql);
 		return $query->result();
+	}
+
+	public function get_mession_info()
+	{
+		$sql = "select c.nomPrenom,v.type,v.matricule,m.* from chauffeur c 
+		join voiture v join mission m on m.id_chauffeur=c.id_chauffeur and m.id_voiture=v.id_voiture";
+		$query = $this->db->query($sql);
+		return $query->result_array();
+	}
+	public function get_one_mession_info($id)
+	{
+		$sql = "select c.nomPrenom,v.type,v.matricule,m.* from chauffeur c join voiture v 
+		join mission m on m.id_chauffeur=c.id_chauffeur and m.id_voiture=v.id_voiture WHERE m.id_mission= " . $id;
+		$query = $this->db->query($sql);
+		return $query->result_array();
 	}
 }
